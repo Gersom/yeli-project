6689b15d93c3c0651331bd87141f344fbeb8cc8b
Gersom Alaja <gersomalaja@gmail.com>
2021-05-10 17:39:51 -0500

    Add change background light or black

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-  .container
+  .container(:class='mode[statusMode]')
     div
       Logo
       h1.title
@@ -8,6 +8,8 @@
         |Youtuber / Streamer
 
       .links
+        button(@click='toogleMode')
+          span Alternar Modo
         ButtonStreamer(nameTo='Lawyer' contentText='Abogada')
         ButtonStreamer(nameTo='Adviser' contentText='Asesora')
 
@@ -18,7 +20,22 @@
 import ButtonStreamer from '~/components/ButtonStreamer'
 
 export default {
-  components: { ButtonStreamer }
+  components: { ButtonStreamer },
+  data () {
+    return {
+      mode: ['white', 'black'],
+      statusMode: 0
+    }
+  },
+  methods: {
+    toogleMode () {
+      this.statusMode += 1
+      if (this.statusMode > (this.mode.length - 1)) {
+        this.statusMode = 0
+      }
+    }
+  }
+
 }
 
 </script>
@@ -26,13 +43,17 @@ export default {
 <style lang='stylus' scoped>
 
   .container
-    background-color: rgb(233, 161, 185)
+    background-color: $clr-primary
     margin: 0 auto;
     min-height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
+  .container.white
+    background: radial-gradient(circle, $clr-primary 0%, $clr-primary2 100%);
+   .container.black
+    background: radial-gradient(circle, $clr-primary-dark 0%, $clr-primary-dark2 50%, $clr-primary-dark3 100%);
 
   .links
     display flex
